7bbb27a6c49a32eca6a9613f0ef647706f66829c
Fy <756292457@qq.com>
2022-02-10 13:08:58 +0800

    add module-body_20220209

---
+++ MLKit-Sample/module-body/.cxx/cmake/debug/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/bytehook/bytehookConfig.cmake
@@ -0,0 +1,7 @@
+add_library(bytehook::bytehook SHARED IMPORTED)
+set_target_properties(bytehook::bytehook PROPERTIES
+    IMPORTED_LOCATION "/Users/fy/.gradle/caches/transforms-3/22c50a3d8d9cb47070655036a29cb284/transformed/jetified-bytehook-1.0.4/prefab/modules/bytehook/libs/android.x86_64/libbytehook.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/fy/.gradle/caches/transforms-3/22c50a3d8d9cb47070655036a29cb284/transformed/jetified-bytehook-1.0.4/prefab/modules/bytehook/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+
---
+++ MLKit-Sample/module-body/src/main/java/com/huawei/mlkit/sample/FyApplication.java
@@ -0,0 +1,28 @@
+package com.huawei.mlkit.sample;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.bytedance.android.bytehook.ByteHook;
+
+public class FyApplication extends Application {
+    private String TAG = "byteHook_fy_java_tag";
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // init bytehook
+        int r = ByteHook.init(new ByteHook.ConfigBuilder()
+                .setMode(ByteHook.Mode.AUTOMATIC)       // 0 自动模式
+//                .setMode(ByteHook.Mode.MANUAL)        // 1 手动模式
+                .setDebug(true)
+                .build());
+        Log.i(TAG, "byteHook init, return: " + r);
+
+        // load hacker
+        System.loadLibrary("hack");
+
+    }
+}
