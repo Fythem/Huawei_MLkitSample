d0538ee15cf35ddc0ccd3f51846270aaae1b3e6a
fy <756292457@qq.com>
2022-02-10 17:19:20 +0800

    Delete MLKit-Sample/module-body/.cxx directory

--- MLKit-Sample/module-body/.cxx/cmake/debug/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/bytehook/bytehookConfig.cmake
+++
@@ -1,7 +0,0 @@
-add_library(bytehook::bytehook SHARED IMPORTED)
-set_target_properties(bytehook::bytehook PROPERTIES
-    IMPORTED_LOCATION "/Users/fy/.gradle/caches/transforms-3/22c50a3d8d9cb47070655036a29cb284/transformed/jetified-bytehook-1.0.4/prefab/modules/bytehook/libs/android.arm64-v8a/libbytehook.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/fy/.gradle/caches/transforms-3/22c50a3d8d9cb47070655036a29cb284/transformed/jetified-bytehook-1.0.4/prefab/modules/bytehook/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-
